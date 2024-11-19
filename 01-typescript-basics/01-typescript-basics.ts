9056d4354d9c79ece182709a635b982cf344c1f7
Kovács Márk <82887808+Smiley1144@users.noreply.github.com>
2024-11-19 17:28:47 +0100

    Add files via upload

---
+++ 01-typescript-basics/01-typescript-basics.ts
@@ -0,0 +1,24 @@
+/* ELSŐ feladat */
+/*  Hozz létre egy Video classt title, uploader és seconds fieldekkel.
+    Legyen egy watch metódusa is, ami kiírja a film címét és a perceket.
+    Hozz létre két példányt a classból és hívd meg mindkettőnek a watch metódusát. */
+
+
+/* MÁSODIK feladat */
+/*  Hozz létre egy Person classt, name és age fieldekkel.
+    Legyen egy describe metódusa, ami visszaadja a fieldek értékét.
+    Hozz létre két példányt, majd hívd meg mindkettőn a describe metódust. */
+
+
+/* HARMADIK feladat*/
+/*  Hozz létre egy User interface-t, name, age és position (feltételes) fieldekkel.
+    Hozz létre egy tömböt ami két user konkrét user objektumot tartalmaz, 
+    majd egy függvény segítségével írd ki a userek nevét és a korukat.  */
+
+
+/* NEGYEDIK feladat */
+/*  Egészítsd ki az első feladatot egy role fielddel (felételes) és hozz létre még 
+    pár usert a listába. A role lehet Admin vagy csak sima User.
+    Egy függvény segítségével vizsgáld meg a usereket role-ok alapján.
+    Ha admin, akkor írd ki a nevét, a korát és a role-t.
+    Ha nem admin, akkor a nevét, a korát és a pozícióját.   */
---
+++ 04-videostore/user-interface.ts
@@ -0,0 +1,47 @@
+// Hozzunk létre egy videó store-t
+
+// Hozzunk létre 5 videót ('Batman', 'Superman', 'Hulk', 'SpiderMan', 'Wonder Woman') 
+// az megjelenési év tetszőleges
+
+
+
+// Hozzunk létre 3 felhasználót ('Tetszőleges névvel és korral'):
+
+
+
+
+// Regisztráljuk a videótárházba a videókat és a usereket...
+
+
+
+// Irjuk ki hány felhasználó van és hány videó a tékában:
+console.log('------------ Sum of videos and Customers ----------');
+
+
+// a 2-es user kölcsönözze ki a Supermant
+
+
+// logozzuk ki hány videó van a 2-es felhasználónak?
+// logozzuk ki a Superman videó állapotát névvel.
+// írjuk ki az összes bent maradt videót címét és kölcsönzési állapotát
+console.log('---------- customer 2 videos and Superman state: ------ ');
+
+
+
+// hozza vissza a user, és vegye ki a SpiderMan-t és a Hulkot.
+// logozzuk ki hány megnézett videója van a 2-es felhasználónak (névvel a felhasználó) 
+// (3 megnézett videójának kell lennie)
+// írjuk ki az összes bent maradt videót címét és kölcsönzési állapotát
+console.log('------- user back Superman, and rent Hulk and SpiderMan videos ------');
+
+
+// Írjuk ki a Hulk és SpiderMan video cimét és kölcsönzési állapotát
+console.log('---------- Video of Hulk and SpiderMan Rentig states: ---------');
+
+
+
+// Bónusz feladat, hozzunk létre a felhasználó osztályunkban egy 'Address' property-t,
+// aminek a típusa egy Address interface.
+// Az interface mezői: 'city' szöveges típussal, 'zipcode' számértékkel, 'country' : szöveges, de
+// nem kötelező mező.
+// A felhasználó osztályban legyen egy setter / getter ennek a property-nek kezelésére
