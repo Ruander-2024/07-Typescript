d15934d16f549df4bcf5ead48261f1c96b1235a1
Mazaine <mazaine89@gmail.com>
2024-11-28 19:39:16 +0100

    11.28/2 bookself fine

--- 02-bookshelf/bookself.js
+++ 02-bookshelf/bookself.js
@@ -22,6 +22,28 @@ var Bookshelf = /** @class */ (function () {
             ? lightestBook
             : currentBook; }, this._books[0]).author;
     };
+    // lightestBook: Az aktuálisan legkönnyebb könyv, amit eddig talált a metódus
+    // currentBook: Az éppen vizsgált könyv
+    // Feltétel: Összehasonlítjuk a könyvek súlyát      lightestBook.bookWeight < currentBook.bookWeight
+    // Ha igaz, akkor a lightestBook megtartja
+    // Ha hamis, akkor lecseréli a lightestBook-ot a currentBook-ra
+    // Az kezdőértékünk a this._books[0]
+    // Ennek a könyvnek, a szerzőjét keressük       .author;
+    Bookshelf.prototype.getAuthorOfMostWrittenPages = function () {
+        if (this._books.length === 0) {
+            return 'Empty bookshelf';
+        }
+        return this._books.reduce(function (longestBook, currentBook) { return longestBook.numberOfPages > currentBook.numberOfPages
+            ? longestBook
+            : currentBook; }, this._books[0]).author;
+    };
+    Bookshelf.prototype.printBooks = function () {
+        if (this._books.length === 0) {
+            console.log('Empty bookshelf');
+            ;
+        }
+        this._books.forEach(function (book) { return console.log(book.getBookInfo()); });
+    };
     return Bookshelf;
 }());
 exports.Bookshelf = Bookshelf;
--- 02-bookshelf/bookself.ts
+++ 02-bookshelf/bookself.ts
@@ -21,4 +21,31 @@ export class Bookshelf{
         ?lightestBook
         :currentBook, this._books[0]).author;
     }
+      // lightestBook: Az aktuálisan legkönnyebb könyv, amit eddig talált a metódus
+        // currentBook: Az éppen vizsgált könyv
+
+        // Feltétel: Összehasonlítjuk a könyvek súlyát      lightestBook.bookWeight < currentBook.bookWeight
+        // Ha igaz, akkor a lightestBook megtartja
+        // Ha hamis, akkor lecseréli a lightestBook-ot a currentBook-ra
+
+        // Az kezdőértékünk a this._books[0]
+
+        // Ennek a könyvnek, a szerzőjét keressük       .author;
+public getAuthorOfMostWrittenPages():string{
+    if(this._books.length === 0){
+        return 'Empty bookshelf';
+    }
+    return this._books.reduce((longestBook, currentBook)=> longestBook.numberOfPages > currentBook.numberOfPages
+    ?longestBook
+    :currentBook, this._books[0]).author
 }
+public printBooks(): void{
+    if(this._books.length === 0){
+        console.log( 'Empty bookshelf');
+        ;
+    }
+    this._books.forEach((book)=> console.log(book.getBookInfo()))
+}
+
+}
+
