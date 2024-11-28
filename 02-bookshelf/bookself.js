4feac0401eaa3cf786515e658ed76fbade912b10
Mazaine <mazaine89@gmail.com>
2024-11-28 18:17:49 +0100

    11.28/1 bookself

--- 02-bookshelf/bookself.js
+++ 02-bookshelf/bookself.js
@@ -1,2 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Bookshelf = void 0;
+var Bookshelf = /** @class */ (function () {
+    function Bookshelf() {
+        this._books = [];
+    }
+    Bookshelf.prototype.addBook = function (book) {
+        this._books.push(book);
+    };
+    Bookshelf.prototype.getBooks = function (year) {
+        if (this._books.length === 0) {
+            return 'Empty bookshelf';
+        }
+        return this._books.filter(function (book) { return book.releaseYear.getFullYear() === year; });
+    };
+    Bookshelf.prototype.getLightestAuthor = function () {
+        if (this._books.length === 0) {
+            return 'Empty bookshelf';
+        }
+        return this._books.reduce(function (lightestBook, currentBook) { return lightestBook.bookWeight < currentBook.bookWeight
+            ? lightestBook
+            : currentBook; }, this._books[0]).author;
+    };
+    return Bookshelf;
+}());
+exports.Bookshelf = Bookshelf;
--- 02-bookshelf/bookself.ts
+++ 02-bookshelf/bookself.ts
@@ -1,1 +1,24 @@
 import { Book } from "./book";
+
+export class Bookshelf{
+    private readonly _books: Book[] = [];
+
+    public addBook(book: Book): void{
+        this._books.push(book);
+    }
+
+    public getBooks(year: number): Book[] | string{
+        if(this._books.length === 0){
+            return 'Empty bookshelf';
+        }
+        return this._books.filter((book) => book.releaseYear.getFullYear() === year)
+    }
+    public getLightestAuthor(): string{
+        if(this._books.length === 0){
+            return 'Empty bookshelf';
+        }
+        return this._books.reduce((lightestBook, currentBook) => lightestBook.bookWeight < currentBook.bookWeight
+        ?lightestBook
+        :currentBook, this._books[0]).author;
+    }
+}
