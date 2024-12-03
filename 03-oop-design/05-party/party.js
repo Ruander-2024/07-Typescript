aa0445ebe26a6014a115f44514d7439b4df5dc0d
Pavity Patrik <pavicpatrik@gmail.com>
2024-12-03 20:17:28 +0100

    12.03

---
+++ 03-oop-design/05-party/party.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Party = void 0;
+var Party = /** @class */ (function () {
+    function Party() {
+        this.heroes = [];
+    }
+    Party.prototype.addHero = function (hero) {
+        this.heroes.push(hero);
+    };
+    Party.prototype.retiredHeroes = function () {
+        if (this.heroes.length > 0) {
+            this.heroes = this.heroes.filter(function (hero) { return hero.money < 5000; });
+        }
+    };
+    Party.prototype.getPayment = function (amount) {
+        if (this.heroes.length > 0) {
+            this.heroes.forEach(function (hero) { return hero.pay(amount); });
+        }
+    };
+    return Party;
+}());
+exports.Party = Party;
