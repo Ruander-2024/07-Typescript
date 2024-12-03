ccf75029b29e5d46107eeff5849fcd0914cde6b9
Peter-Kohlrusz <peter.kohlrusz@gmail.com>
2024-12-03 18:38:57 +0100

    12.03 Heroes class

---
+++ 03-oop-design/05-party/heroes.ts
@@ -0,0 +1,52 @@
+export abstract class Hero {
+    constructor(protected name: string, protected _money: number) {
+        
+    }
+
+    public status (): string{
+        return`${this.name} has ${this._money} gold.`
+    }
+
+    public pay(amount: number): void{
+        this._money = this._money + amount; // this._money +=amount
+    }
+
+    get money(){
+        return this._money;
+    }
+
+}
+
+export class Fighter extends Hero{
+    constructor(name: string, private readonly armorColor: string){
+        super(name, 0);
+    }
+
+    public status(): string {
+        return `${this.name} has ${this._money} gold and a/an ${this.armorColor} armor`
+    }
+
+}
+
+export class Bard extends Hero{
+    constructor(name: string){
+        super(name, 200);
+    }
+    public pay(amount: number){
+        this._money = this._money + Math.floor(amount * 0.8);  // this._money += Math.floor(amount * 0.8);
+    }
+}
+
+export class Warlock extends Hero{
+    constructor(name: string, private readonly demonName: string){
+        super(name, 0);
+    }
+
+    public status(): string{
+        return `${this.name} has ${this._money} gold and works for ${this.demonName}.`
+    }
+
+    public pay(amount: number): void{
+        this._money = this._money += Math.floor(amount * 1.1);   // this._money += Math.floor(amount * 1.1)
+    }
+}
