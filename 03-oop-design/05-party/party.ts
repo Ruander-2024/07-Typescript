0e8329b2a0ca7fc60857c69724c0d2fd57a61770
Smiley1144 <kovacsm519@gmail.com>
2024-12-03 21:12:35 +0100

    party.ts

---
+++ 03-oop-design/05-party/party.ts
@@ -0,0 +1,21 @@
+import { Hero } from "./heroes";
+
+export class Party{
+    private heroes: Hero[] = [];
+
+    public addHero(hero: Hero): void{
+        this.heroes.push(hero);
+    }
+
+    public retireHeroes(): void{
+        if(this.heroes.length > 0){
+            this.heroes = this.heroes.filter(hero => hero.money < 5000);
+        }
+    }
+
+    public getPayment(amount: number): void{
+        if(this.heroes.length > 0){
+            this.heroes.forEach(hero => hero.pay(amount));
+        }
+    }
+}
